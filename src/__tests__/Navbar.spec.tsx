fa4787ed4d914b8d4ca334897297af53fca7fd38
Filipe <femotta96@gmail.com>
2023-03-17 14:38:22 -0300

    feat: add Navbar component tests

---
+++ src/__tests__/Navbar.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Navbar } from "../components/Navbar/Navbar";
+
+describe("Navbar component", () => {
+  it("renders without crashing", () => {
+    render(<Navbar />);
+  });
+
+  it("toggles mobile menu when menu button is clicked", () => {
+    render(<Navbar />);
+    const menuButton = screen.getByTestId("menu-button");
+    const mobileMenu = screen.getByTestId("mobile-menu");
+
+    if (mobileMenu) {
+      expect(mobileMenu.classList).not.toContain("responsive-nav");
+    }
+
+    fireEvent.click(menuButton);
+
+    expect(mobileMenu.classList).not.toContain("responsive-nav");
+  });
+
+  it("closes mobile menu when close button is clicked", () => {
+    render(<Navbar />);
+    const menuButton = screen.getByTestId("menu-button");
+    const closeButton = screen.getByTestId("close-button");
+    const mobileMenu = screen.getByTestId("mobile-menu");
+
+    expect(mobileMenu).toBeInTheDocument();
+    expect(mobileMenu).toHaveClass("mobile-menu");
+
+    fireEvent.click(menuButton);
+
+    expect(mobileMenu).not.toHaveClass("responsive-nav");
+
+    fireEvent.click(closeButton);
+
+    expect(mobileMenu).not.toHaveClass("responsive-nav");
+  });
+});
