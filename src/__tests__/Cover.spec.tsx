ed961d7121071c7b74f59d19c88e8e9af4a91dea
Filipe <femotta96@gmail.com>
2023-03-21 08:49:14 -0300

    feat: Add new tests to test file

--- src/__tests__/Cover.spec.tsx
+++ src/__tests__/Cover.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Cover } from '../container/Cover/Cover';
 
 describe('Cover container', () => {
@@ -8,5 +8,20 @@ describe('Cover container', () => {
     const subtitle = getByText(/A FLORESTA-ÁGUA/i);
     expect(title).toBeInTheDocument();
     expect(subtitle).toBeInTheDocument();
+  });
+
+  it('changes background color on scroll', async () => {
+    render(<Cover />);
+    const headerElement = screen.getByRole('section');
+    expect(headerElement).toHaveClass('emerald');
+
+    window.pageYOffset = 10;
+    window.dispatchEvent(new Event('scroll'));
+
+    // Wait for the scroll to complete and check the class
+    await waitFor(() => {
+      expect(window.pageYOffset).toBe(10);
+      expect(headerElement).toHaveClass('beige');
+    });
   });
 });
